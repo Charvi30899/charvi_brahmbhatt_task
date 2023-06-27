e101946c20ebccda1dfd6d78583d508985efae76
Charvi30899 <charvi.brahmbhatt30@gmail.com>
2023-06-27 09:25:44 +0530

    Initial commit

---
+++ lib/view/room_list_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RoomSelectView extends StatefulWidget {
+  const RoomSelectView({super.key});
+
+  @override
+  State<RoomSelectView> createState() => _RoomSelectViewState();
+}
+
+class _RoomSelectViewState extends State<RoomSelectView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
